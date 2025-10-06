b351a6699458c84cfe4d4d8885d611400573bbd5
yixing <yanjizhou312@163.com>
2025-10-07 05:25:39 +0800

    add aubo and update robot type

--- examples/jaka_dual/src/main.rs
+++ examples/jaka_dual/src/main.rs
@@ -1,7 +1,19 @@
-use libjaka::JakaRobot;
+use libjaka::{JakaMini2, types::SetTioVoutParamData};
 use robot_behavior::behavior::*;
 fn main() {
-    let (mut robot_1, mut robot_2) = (JakaRobot::new(""), JakaRobot::new(""));
-    robot_1.enable().unwrap();
-    robot_2.enable().unwrap();
+    // let (mut robot_1, mut robot_2) = (JakaMini2::new(""), JakaMini2::new(""));
+    // robot_1.enable().unwrap();
+    // robot_2.enable().unwrap();
+    let mut robot_1 = JakaMini2::new("192.168.1.100");
+    let _ = robot_1.enable();
+    // let _ = robot_1.disable();
+    // robot_1.move_joint_rel(&[0.1; 6]).unwrap();
+    robot_1
+        ._set_tio_vout_param(SetTioVoutParamData {
+            tio_vout_ena: 1,
+            tio_vout_vol: 0,
+        })
+        .unwrap();
+    let tio = robot_1._get_tio_vout_param().unwrap();
+    println!("{:?}", tio);
 }
