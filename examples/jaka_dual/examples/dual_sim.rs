0012d2436e9628a5b23d59b3ce91d4728df72532
yixing <yanjizhou312@163.com>
2025-10-19 16:54:24 +0800

    add urdf files

---
+++ examples/jaka_dual/examples/dual_sim.rs
@@ -0,0 +1,33 @@
+use libjaka::JakaMini2;
+use robot_behavior::behavior::*;
+use rsbullet::{BulletResult, Mode, RsBullet};
+use std::{f64::consts::FRAC_PI_4, thread::sleep, time::Duration};
+
+fn main() -> BulletResult<()> {
+    let mut physics = RsBullet::new(Mode::Gui)?;
+    physics
+        .set_additional_search_path("E:\\yixing\\code\\Robot-Exp\\drives\\asserts")?
+        .set_gravity([0., 0., -10.])?
+        .set_step_time(Duration::from_secs_f64(1. / 240.))?;
+    let mut robot_1 = physics
+        .robot_builder::<JakaMini2>()
+        .base([0.0, 0.2, 0.0])
+        .base_fixed(true)
+        .load()?;
+    let mut robot_2 = physics
+        .robot_builder::<JakaMini2>()
+        .base([0.0, -0.2, 0.0])
+        .base_fixed(true)
+        .load()?;
+
+    for _ in 0..100_000 {
+        physics.step()?;
+    }
+    robot_1.move_joint(&[0.; 6])?;
+    robot_2.move_joint(&[FRAC_PI_4; 6])?;
+    loop {
+        physics.step()?;
+
+        sleep(Duration::from_secs_f64(0.01));
+    }
+}
