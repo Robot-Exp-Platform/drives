90c6c17917bd921c3ec25ad2764a40c3824113c3
yixing <yanjizhou312@163.com>
2025-10-21 14:09:05 +0800

    success running!

--- examples/jaka_dual/examples/dual_sim.rs
+++ examples/jaka_dual/examples/dual_sim.rs
@@ -1,21 +1,23 @@
+use anyhow::Result;
 use libjaka::JakaMini2;
 use robot_behavior::behavior::*;
-use rsbullet::{BulletResult, Mode, RsBullet};
+use rsbullet::{Mode, RsBullet};
 use std::{f64::consts::FRAC_PI_4, thread::sleep, time::Duration};
 
-fn main() -> BulletResult<()> {
+fn main() -> Result<()> {
     let mut physics = RsBullet::new(Mode::Gui)?;
     physics
         .set_additional_search_path("E:\\yixing\\code\\Robot-Exp\\drives\\asserts")?
         .set_gravity([0., 0., -10.])?
         .set_step_time(Duration::from_secs_f64(1. / 240.))?;
+
     let mut robot_1 = physics
-        .robot_builder::<JakaMini2>()
+        .robot_builder::<JakaMini2>("robot_1")
         .base([0.0, 0.2, 0.0])
         .base_fixed(true)
         .load()?;
     let mut robot_2 = physics
-        .robot_builder::<JakaMini2>()
+        .robot_builder::<JakaMini2>("robot_2")
         .base([0.0, -0.2, 0.0])
         .base_fixed(true)
         .load()?;
