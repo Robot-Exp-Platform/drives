4f53bcf4ecb2295402096051b3ce974240589c24
yixing <yanjizhou312@163.com>
2026-04-03 05:48:34 +0800

    add unitree go

---
+++ examples/jaka_dual/examples/multi_sim.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use franka_rust::FrankaEmika;
+use robot_behavior::{MotionType, behavior::*};
+use rsbullet::{Mode, RsBullet};
+use std::{thread::sleep, time::Duration};
+
+fn main() -> Result<()> {
+    let mut physics = RsBullet::new(Mode::Gui)?;
+    physics
+        .add_search_path("./asserts")?
+        .set_gravity([0., 0., -10.])?
+        .set_step_time(Duration::from_secs_f64(1. / 240.))?;
+
+    let mut robots = Vec::new();
+    for i in 0..5 {
+        let mut row = Vec::new();
+        for j in 0..5 {
+            let robot = physics
+                .robot_builder::<FrankaEmika>(format!("robot_{}_{}", i, j))
+                .base_fixed(true)
+                .base([i as f64, j as f64, 0.])
+                .load()?;
+            row.push(robot);
+        }
+        robots.push(row);
+    }
+
+    for robot_l in robots {
+        for mut robot in robot_l {
+            let mut t = 0.0_f64;
+            let dt = 1.0_f64 / 240.0_f64;
+
+            robot.move_with_closure(move |_, _| {
+                t += dt;
+                (MotionType::Joint([t.sin(); 7]), false)
+            })?;
+
+            // robot.move_joint(&FrankaEmika::JOINT_DEFAULT)?;
+        }
+    }
+
+    loop {
+        physics.step()?;
+        sleep(Duration::from_secs_f64(0.01));
+    }
+}
--- examples/jaka_dual/examples/unitree_go.rs
+++ examples/jaka_dual/examples/unitree_go.rs
@@ -1,22 +1,24 @@
 use anyhow::Result;
-use libjaka::JakaA5;
-use robot_behavior::behavior::*;
-use rsbullet::{Mode, RsBullet, RsBulletRobot};
+use libunitree::UnitreeGo2;
+use robot_behavior::{behavior::*, roplat_data_dir};
+use rsbullet::{Mode, RsBullet};
 use std::{thread::sleep, time::Duration};
 
 fn main() -> Result<()> {
+    let assets_dir = roplat_data_dir().unwrap();
+
     let mut physics = RsBullet::new(Mode::Gui)?;
     physics
-        .add_search_path("./asserts")?
+        .add_search_path(&assets_dir)?
         .set_gravity([0., 0., -10.])?
         .set_step_time(Duration::from_secs_f64(1. / 240.))?;
 
-    let mut robot_1: RsBulletRobot<JakaA5> = physics
-        .robot_builder::<JakaA5>("robot_1")
+    let _ = physics
+        .robot_builder::<UnitreeGo2>("robot_1")
         .base_fixed(true)
         .load()?;
 
-    robot_1.move_joint(&JakaA5::JOINT_DEFAULT)?;
+    // robot_1.move_joint(&JakaA5::JOINT_DEFAULT)?;
 
     loop {
         physics.step()?;
